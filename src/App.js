2436bcd09eed625f976ebefe5f921ee603b26a14
WEICHUENLEE <leeweichuen23@gmail.com>
2024-06-11 22:22:15 +0800

    Implementing time travel

--- src/App.js
+++ src/App.js
@@ -87,17 +87,19 @@ function Board({ XIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-    const [XIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
-    const currentSquares = history[history.length - 1];
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
 
     function handlePlay(nextSquares) {
-        setHistory([...history, nextSquares]);
-        setXIsNext(!XIsNext);
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
     }
 
     function jumpTo(nextMove) {
-        // TODO
+        setCurrentMove(nextMove);
     }
 
     const moves = history.map((squares, move) => {
@@ -110,7 +112,7 @@ export default function Game() {
         }
 
         return (
-            <li>
+            <li key={move}>
                 <button onClick={() => jumpTo(move)}> {description} </button>
             </li>
         );
